f597276a90cb92e02ae2e966d2c4e140771dd10c
Dinhhai2002 <88237144+Dinhhai2002@users.noreply.github.com>
2022-12-06 12:36:57 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,362 @@
+
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeRegressor
+import pickle
+import numpy as np
+import streamlit as st
+from streamlit_option_menu import option_menu
+from sklearn.linear_model import LogisticRegression
+import plost
+
+
+# loading the saved models
+diabetes_dataset = pd.read_csv('./dataset/diabetes.csv') 
+heart_disease_dataset = pd.read_csv('./dataset/heart.csv') 
+parkinsons_dataset = pd.read_csv('./dataset/parkinsons.csv') 
+
+diabetes_model = pickle.load(open('./saved models/diabetes_model.sav', 'rb'))
+
+heart_disease_model = pickle.load(open('./saved models/heart_disease_model.sav','rb'))
+
+parkinsons_model = pickle.load(open('./saved models/parkinsons_model.sav', 'rb'))
+
+
+
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction','contact','admin'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+
+    st.title('Diabetes Prediction using ML')
+    
+    
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+    #Thông số Accuracy của tập dữ liệu
+    X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+    Y = diabetes_dataset['Outcome']
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+    classifier = svm.SVC(kernel='linear')
+    classifier.fit(X_train, Y_train)
+    X_train_prediction = classifier.predict(X_train)
+    training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+    st.write(f'Accuracy score of the training data =', training_data_accuracy)
+    X_test_prediction = classifier.predict(X_test)
+    test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+    st.write(f'Accuracy score of the test data =', test_data_accuracy)
+   
+    diab_diagnosis = ''
+    
+    
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+   
+
+
+
+
+# Heart Disease Prediction Page
+if (selected == 'Heart Disease Prediction'):
+    
+    # page title
+    st.title('Heart Disease Prediction using ML')
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        age = st.text_input('Age')
+        
+    with col2:
+        sex = st.text_input('Sex')
+        
+    with col3:
+        cp = st.text_input('Chest Pain types')
+        
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+        
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+        
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+        
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+        
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+        
+    with col1:
+        OLDPEAK = st.text_input('ST depression induced by exercise')
+        
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+        
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+        
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+
+
+    X = heart_disease_dataset.drop(columns = 'target', axis=1)
+    Y = heart_disease_dataset['target']
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+    classifier = LogisticRegression()
+    classifier.fit(X_train, Y_train)
+    X_train_prediction = classifier.predict(X_train)
+    training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+    st.write(f'Accuracy score of the training data =', training_data_accuracy)
+    X_test_prediction = classifier.predict(X_test)
+    test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+    st.write(f'Accuracy score of the test data =', test_data_accuracy)
+        
+     
+     
+   
+    heart_diagnosis = ''
+    
+   
+    
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = heart_disease_model.predict([np.array([age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,OLDPEAK,slope,ca,thal],dtype=float)])                          
+        
+        if (heart_prediction[0] == 1):
+          heart_diagnosis = 'The person is having heart disease'
+        else:
+          heart_diagnosis = 'The person does not have any heart disease'
+        
+    st.success(heart_diagnosis)
+        
+    
+    
+
+# Parkinson's Prediction Page
+if (selected == "Parkinsons Prediction"):
+    
+    # page title
+    st.title("Parkinson's Disease Prediction using ML")
+    
+    col1, col2, col3, col4, col5 = st.columns(5)  
+    
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+        
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+        
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+        
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+        
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+        
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+        
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+        
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+        
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+        
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+        
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+        
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+        
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+        
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+        
+    with col5:
+        NHR = st.text_input('NHR')
+        
+    with col1:
+        HNR = st.text_input('HNR')
+        
+    with col2:
+        RPDE = st.text_input('RPDE')
+        
+    with col3:
+        DFA = st.text_input('DFA')
+        
+    with col4:
+        spread1 = st.text_input('spread1')
+        
+    with col5:
+        spread2 = st.text_input('spread2')
+        
+    with col1:
+        D2 = st.text_input('D2')
+        
+    with col2:
+        PPE = st.text_input('PPE')
+        
+    X = parkinsons_dataset.drop(columns = ['name','status'], axis=1)
+    Y = parkinsons_dataset['status']
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+    classifier = svm.SVC(kernel='linear')
+    classifier.fit(X_train, Y_train)
+    X_train_prediction = classifier.predict(X_train)
+    training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+    st.write(f'Accuracy score of the training data =', training_data_accuracy)
+    X_test_prediction = classifier.predict(X_test)
+    test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+    st.write(f'Accuracy score of the test data =', test_data_accuracy)
+    
+  
+    parkinsons_diagnosis = ''
+    
+       
+    if st.button("Parkinson's Test Result"):
+        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
+        
+        if (parkinsons_prediction[0] == 1):
+          parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+          parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        
+    st.success(parkinsons_diagnosis)
+
+if (selected == 'contact'):
+    contact_form = """
+    <form action="https://formsubmit.co/haitranvipqt@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here"></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    st.markdown(contact_form, unsafe_allow_html=True)
+
+    #link file css
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+    local_css("style.css")
+if (selected=='admin'):
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    st.sidebar.header('Dashboard ')
+
+    
+    time_hist_color = st.sidebar.selectbox('`Color by`', ('temp_min', 'temp_max')) 
+
+    st.sidebar.subheader('Donut chart parameter')
+    donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+
+    st.sidebar.subheader('Line chart parameters')
+    plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+    plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+
+    
+    
+
+
+    
+    st.markdown('### Metrics')
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col2.metric("Wind", "9 mph", "-8%")
+    col3.metric("Humidity", "86%", "4%")
+
+    
+    seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+    stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+
+    c1, c2 = st.columns((7,3))
+    with c1:
+        st.markdown('### Heatmap')
+        plost.time_hist(
+        data=seattle_weather,
+        date='date',
+        x_unit='week',
+        y_unit='day',
+        color=time_hist_color,
+        aggregate='median',
+        legend=None,
+        height=345,
+        use_container_width=True)
+    with c2:
+        st.markdown('### Donut chart')
+        plost.donut_chart(
+            data=stocks,
+            theta=donut_theta,
+            color='company',
+            legend='bottom', 
+            use_container_width=True)
+
+    # Row C
+    st.markdown('### Line chart')
+    st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
+
+
